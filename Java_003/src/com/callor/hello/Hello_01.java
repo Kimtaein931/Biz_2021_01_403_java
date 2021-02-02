eab02d0c915e4ac159b8a10492af7818f470f625
Kimtaein931 <ta2nk@naver.com>
2021-02-02 14:27:17 +0900

    first

---
+++ Java_003/src/com/callor/hello/Hello_01.java
@@ -0,0 +1,19 @@
+package com.callor.hello;
+
+public class Hello_01 {
+	public static void main(String[] args) {
+		
+		System.out.println("여기는 Hello_01 Class");
+		System.out.println("여기는 Hello_O1 Class");
+		System.out.println();
+		
+		System.out.print("30 + 40 = ");
+		// ln -> 줄바꿈
+		System.out.println(30 + 40);
+		System.out.println();
+		System.out.print("Republic ");
+		System.out.print("Of ");
+		System.out.println("Korea");
+		
+	}
+}
