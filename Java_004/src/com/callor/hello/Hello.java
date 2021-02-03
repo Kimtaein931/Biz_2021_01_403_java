03dafcdb7bb577bf0182331ea2e27c79eb6b92bf
Kimtaein931 <ta2nk@naver.com>
2021-02-03 09:30:55 +0900

    README 파일 수정

--- Java_004/src/com/callor/hello/Hello.java
+++ Java_004/src/com/callor/hello/Hello.java
@@ -1,5 +1,21 @@
 package com.callor.hello;
 
 public class Hello {
-
+ 
+	public static void main(String[] args) {
+		
+		int num1 = 55;
+		int num2 = 68;
+		// int(정수)형 변수 선언
+		
+		
+		System.out.println(num1 + num2);
+		System.out.println(num1 * num2);
+		System.out.println(num2 - num1);
+		System.out.println(num2 / num1);
+		System.out.println(num2 % num1);
+		
+	}
+	
+	
 }
