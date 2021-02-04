771e2d151ad93fd04efdec7a9da3b818a2bde015
Kimtaein931 <ta2nk@naver.com>
2021-02-04 16:11:14 +0900

    반복문

--- Java_006/src/com/callor/varriable/Loop_04.java
+++ Java_006/src/com/callor/varriable/Loop_04.java
@@ -28,8 +28,24 @@ public class Loop_04 {
 			num1 += 1;
 		}
 		System.out.println("----------");
-		for (num1 = 0; num1 <= 5; num1 += 1) {
-			System.out.println(num1);
+		/*
+		 * for() 명령문이 실행되는 순서
+		 * 최초에 한 번 1 실행해서 num1을 1로 세팅
+		 * 2 실행해서 num1 < 10 물어보기
+		 * 결과가 true면 3 ~ 7 순서대로 실행
+		 * 8번 실행해서 num1을 1 증가
+		 * 
+		 * 다시 2를 실행해서 num1 < 10 물어보기
+		 * 이렇게 반복하다가 num1 < 10 false가 될 때까지
+		 */
+		// 1 // 2 // 8
+		for (num1 = 1; num1 < 10; num1 += 1) {
+			System.out.print(intDan); // 3
+			System.out.print(" x "); // 4
+			System.out.print(num1); // 5
+			System.out.print(" = "); // 6
+			System.out.println(intDan * num1); // 7
+
 		}
 	}
 }
