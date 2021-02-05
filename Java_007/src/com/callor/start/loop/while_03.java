42c161a56e8de6cbb90a9fdaddbc58e960ba6eb7
Kimtaein931 <ta2nk@naver.com>
2021-02-05 16:42:31 +0900

    While()

--- Java_007/src/com/callor/start/loop/while_03.java
+++ Java_007/src/com/callor/start/loop/while_03.java
@@ -6,7 +6,12 @@ public class while_03 {
 
 		int index = 0;
 		while (true) {
-
+			// 매우 불편한 코드
+			if(index % 3 > 0) {
+				System.out.println("3의 배수가 아님");
+			}
+			
+			// 정상적인 코드
 			if (++index % 3 == 0) {
 				System.out.println(index + " 는 3의 배수");
 			} else {
