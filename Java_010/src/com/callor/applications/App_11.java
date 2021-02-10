80160d2e8cf6c786aeffdd96176d0c2df964705b
Kimtaein931 <ta2nk@naver.com>
2021-02-10 14:32:45 +0900

    클래스

--- Java_010/src/com/callor/applications/App_11.java
+++ Java_010/src/com/callor/applications/App_11.java
@@ -9,21 +9,19 @@ public class App_11 {
 	public static void main(String[] args) {
 		MyGuGuDan myDan = new MyGuGuDan();
 		Scanner scan = new Scanner(System.in);
-		while(true) {
+		while (true) {
 			System.out.print("숫자 (2 ~ 9), -1 : Quit >> ");
 			int num = scan.nextInt();
-			if(num == -1) {
+			if (num == -1) {
 				System.out.println("GAME OVER");
 				break;
 			}
-			if(num < 2 || num > 9) {
+			if (num < 2 || num > 9) {
 				System.out.println("다시 입력하십시오.");
 				System.out.println("2 ~ 9 안에서 입력 바랍니다.");
-			}else {
+			} else {
 				myDan.print(num);
 			}
-			
 		}
 	}
-	
 }
