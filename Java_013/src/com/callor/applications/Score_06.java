dc5ccbd6ba7404f527b8e916509dd94dbc762104
Kimtaein931 <ta2nk@naver.com>
2021-02-19 16:20:13 +0900

    Array

--- Java_013/src/com/callor/applications/Score_06.java
+++ Java_013/src/com/callor/applications/Score_06.java
@@ -1,12 +1,35 @@
 package com.callor.applications;
 
+import java.util.Scanner;
+
 public class Score_06 {
 
 	public static void main(String[] args) {
-		
+
+		Scanner scan = new Scanner(System.in);
 		String[] strSubject = new String[3];
-		
-		
+
+		strSubject[0] = "국어";
+		strSubject[1] = "수학";
+		strSubject[2] = "영어";
+
+		int[] intScore = new int[3];
+
+		System.out.println("각 과목의 성적을 입력하세요.");
+
+		for (int i = 0; i < intScore.length; i++) {
+			System.out.print(strSubject[i] + " >> ");
+			intScore[i] = scan.nextInt();
+		}
+
+		int scoreSum = 0;
+
+		for (int i = 0; i < intScore.length; i++) {
+			scoreSum += intScore[i];
+		}
+
+		System.out.printf("과목 점수의 합계 : %d점", scoreSum);
+
 	}
-	
+
 }
