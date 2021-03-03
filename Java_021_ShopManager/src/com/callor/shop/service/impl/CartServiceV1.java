a9ee4ae8cace736431d40f250e51994343a766e0
Kimtaein931 <ta2nk@naver.com>
2021-03-03 15:28:05 +0900

    shop

--- Java_021_ShopManager/src/com/callor/shop/service/impl/CartServiceV1.java
+++ Java_021_ShopManager/src/com/callor/shop/service/impl/CartServiceV1.java
@@ -7,16 +7,20 @@ import java.util.Scanner;
 import com.callor.shop.model.CartVO;
 import com.callor.shop.service.CartService;
 
+// CartServiceV1에서 CartService 인터페이스를 implements 함 
 public class CartServiceV1 implements CartService {
 
+	// 인스턴스 변수 선언'만'
 	private List<CartVO> cartList;
 	private Scanner scan;
 
+	// 생성자 method에서 초기화
 	public CartServiceV1() {
 		cartList = new ArrayList<CartVO>();
 		scan = new Scanner(System.in);
 	}
 
+	// 구매자, 상품, 단가, 수량을 입력하는 inputCart() method 생성
 	public void inputCart() {
 
 		System.out.println("=========================================");
@@ -35,15 +39,17 @@ public class CartServiceV1 implements CartService {
 			String strPrice = scan.nextLine();
 			try {
 				intPrice = Integer.valueOf(strPrice);
+				// 입력한 단가가 1 미만이라면
 				if (intPrice < 1) {
 					System.out.println("단가는 1 이상 입력하십시오.");
 				} else {
 					break;
 				}
 			} catch (Exception e) {
+				// 입력한 단가가 정수가 아니라면
 				System.out.println("단가는 숫자로 입력바랍니다.");
 			}
-		}
+		}// while end
 
 		// 수량을 담을 정수형 변수 선언
 		int intQty = 0;
@@ -53,34 +59,42 @@ public class CartServiceV1 implements CartService {
 			String strQty = scan.nextLine();
 			try {
 				intQty = Integer.valueOf(strQty);
+				// 입력한 수량이 1 미만이라면
 				if (intQty < 1) {
 					System.out.println("수량은 1 이상 입력하십시오.");
 				} else {
 					break;
 				}
 			} catch (Exception e) {
+				// 입력한 수량이 정수가 아니라면
 				System.out.println("수량은 1 ~ 3 정수로 입력바랍니다.");
 			}
-		}
+		}// while end
 
+		// CartVO를 새롭게 선언한 뒤 고객, 상품, 단가, 수량, 합계 값 넘김
 		CartVO cartVO = new CartVO();
 		cartVO.setUserName(strUserName); // 고객 이름
 		cartVO.setProductName(strPName); // 상품 이름
 		cartVO.setPrice(intPrice); // 상품 단가
 		cartVO.setQty(intQty); // 상품 수량
-		cartVO.setTotal(intPrice * intQty);
+		cartVO.setTotal(intPrice * intQty); // 합계
+		
+		// cartList 배열에 cartVO 값 넘김
 		cartList.add(cartVO);
 	}
 
 	public void printCart() {
-		int nSize = cartList.size();
-		int pCount = 0;
-		int pSum = 0;
 		System.out.println("* 전체 장바구니 리스트");
 		System.out.println("=========================================");
 		System.out.println("구매자\t상품명\t단가\t수량\t합계");
 		System.out.println("-----------------------------------------");
+		
+		// CartList에 들어있는 배열 개수를 세어서 저장
+		int nSize = cartList.size();
+		int pCount = 0;
+		int pSum = 0;
 		for (int i = 0; i < nSize; i++) {
+			// 전체 합계값을 알기 위해 pSum 변수에 누적대입
 			pSum += cartList.get(i).getTotal();
 			System.out.printf("%s\t%s\t%d\t%d\t%d\n", 
 					cartList.get(i).getUserName(), 
@@ -88,6 +102,7 @@ public class CartServiceV1 implements CartService {
 					cartList.get(i).getPrice(),
 					cartList.get(i).getQty(),
 					cartList.get(i).getTotal());
+			// 상품수량을 알기 위해 pCount값 1씩 증가
 			pCount++;
 		}
 		System.out.println("=========================================");
@@ -104,7 +119,10 @@ public class CartServiceV1 implements CartService {
 		int pSum = 0;
 		System.out.println(username + " 장바구니 리스트");
 		for (int i = 0; i < nSize; i++) {
+			// 입력한 username과 일치한 값이 있을 경우 실행
 			if (username.equals(cartList.get(i).getUserName())) {
+				
+				// 전체 합계값
 				pSum += cartList.get(i).getTotal();
 				System.out.printf("%s\t%s\t%d\t%d\t%d\n", 
 						cartList.get(i).getUserName(),
@@ -112,6 +130,8 @@ public class CartServiceV1 implements CartService {
 						cartList.get(i).getPrice(),
 						cartList.get(i).getQty(),
 						cartList.get(i).getTotal());
+				
+				// 상품수량을 알기 위해 pCount값 1씩 증가
 				pCount++;
 			}
 		}
--- Java_021_ShopManager/src/com/callor/shop/service/impl/MenuServiceV1.java
+++ Java_021_ShopManager/src/com/callor/shop/service/impl/MenuServiceV1.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 import com.callor.shop.service.MenuService;
 
+// MenuServiceV1 클래스에서 MenuService 인터페이스를 implements 함
 public class MenuServiceV1 implements MenuService {
 
+	// 인스턴수 변수를 선언'만'
 	private Scanner scan;
 
 	public MenuServiceV1() {
@@ -13,10 +15,10 @@ public class MenuServiceV1 implements MenuService {
 		scan = new Scanner(System.in);
 	}
 
+	// 반환타입이 정수형인 seletmenu() method 생성
 	public Integer selectmenu() {
 
 		while (true) {
-
 			System.out.println("=========================================");
 			System.out.println("빛나라 쇼핑몰 2021");
 			System.out.println("-----------------------------------------");
@@ -27,6 +29,7 @@ public class MenuServiceV1 implements MenuService {
 			System.out.println("=========================================");
 			System.out.print("선택 >> ");
 			String strMenu = scan.nextLine();
+			// 키보드로 QUIT를 입력하면 장바구니 끝내기(종료)
 			if (strMenu.equals("QUIT")) {
 				System.out.println("장바구니를 종료합니다.");
 				break;
@@ -35,6 +38,7 @@ public class MenuServiceV1 implements MenuService {
 				try {
 					Integer intMenu = Integer.valueOf(strMenu);
 					if (intMenu >= 1 && intMenu <= 3) {
+						// 입력한 값이 1 ~ 3 이라면 intMenu값 return
 						return intMenu;
 					} else {
 						System.out.println("1 ~ 3 사이의 숫자를 입력해주십시오.");
